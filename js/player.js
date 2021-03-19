6f71b9a4e0986e0832e1def4710e385b6658f9aa
smartprogrammer2 <73342023+smartprogrammer2@users.noreply.github.com>
2021-03-19 16:04:01 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,53 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static scoreChange1() {
+        player1.score += 1;
+        database.ref('/').update({
+            score: player.score
+        })
+    }
+
+    static scoreChange2() {
+        player2.score += 1;
+        database.ref('/').update({
+            score: player.score
+        })
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+ 
+}
